fb1f0ff455106cc609f6dd88bafd2da0f16f666e
saravanaselvangit <46033682+saravanaselvangit@users.noreply.github.com>
2019-04-09 17:35:56 -0700

    Create ZipRange.java

---
+++ ZipRange.java
@@ -0,0 +1,23 @@
+public class ZipRange {
+	
+	private Integer start;
+	
+	public Integer getStart() {
+		return start;
+	}
+
+	public void setStart(Integer start) {
+		this.start = start;
+	}
+
+	public Integer getEnd() {
+		return end;
+	}
+
+	public void setEnd(Integer end) {
+		this.end = end;
+	}
+
+	private Integer end;
+
+}
